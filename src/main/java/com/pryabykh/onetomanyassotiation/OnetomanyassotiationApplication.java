db678e743d22bf56505b900f8d57bdbdaebcddcb
Pavel Ryabykh <pryabykh@gmail.com>
2023-04-29 16:36:49 +0300

    Initial commit

---
+++ src/main/java/com/pryabykh/onetomanyassotiation/OnetomanyassotiationApplication.java
@@ -0,0 +1,13 @@
+package com.pryabykh.onetomanyassotiation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetomanyassotiationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnetomanyassotiationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pryabykh/onetomanyassotiation/OnetomanyassotiationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pryabykh.onetomanyassotiation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetomanyassotiationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
